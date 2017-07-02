8eb6aa3f3c82da4d6b7f60396fdb12ab7aff024e
pchela911 <pchelena911@gmail.com>
2017-07-02 11:18:11 +0300

    Plugin gulp-imagemin for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 // в этой секции подключаем все необходимые плагины
 var gulp = require('gulp'),  
     uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin');
 
 // здесь вызываем необходимую задачу
 gulp.task('scripts', function () {  
@@ -15,3 +16,15 @@ gulp.task('styles', function() {
     .pipe(concat('out.css'))
     .pipe(gulp.dest('build/css'));
 });
+
+gulp.task('default', () =>
+	gulp.src('img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/img'))
+);
+// Можно ли так поставить задачу, что бы одновременно картинки из корневой папки img минифицировлись и складывались в build/img, а иконки из img/icons, соответственно в build/img/icons? Или только делить на две задачи, как у меня?
+gulp.task('image', () =>
+	gulp.src('img/icons/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/img/icons'))
+);

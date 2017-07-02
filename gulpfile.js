a2082f9d308414f34e6a325957ea437ebb7330f4
pchela911 <pchelena911@gmail.com>
2017-07-02 11:53:17 +0300

    Plugin gulp-svg2png

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@
 var gulp = require('gulp'),  
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    svg2png = require('gulp-svg2png');
 
-// здесь вызываем необходимую задачу
 gulp.task('scripts', function () {  
     gulp.src('js/main.js')
         .pipe(uglify())
@@ -22,9 +22,15 @@ gulp.task('image', () =>
 		.pipe(imagemin())
 		.pipe(gulp.dest('build/img'))
 );
-// Можно ли так поставить одну задачу, что бы одновременно картинки из корневой папки img минифицировлись и складывались в build/img, а иконки из img/icons, соответственно в build/img/icons? Или только делить на две задачи, как у меня?
 gulp.task('icons', () =>
 	gulp.src('img/icons/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('build/img/icons'))
 );
+// Можно ли так поставить одну задачу, что бы одновременно картинки из корневой папки img минифицировлись и складывались в build/img, а иконки из img/icons, соответственно в build/img/icons? Или только делить на две задачи, как у меня?
+
+gulp.task('svg2png', function () {
+    gulp.src('img/icons/**/*.svg')
+        .pipe(svg2png())
+        .pipe(gulp.dest('build/img/icons'));
+});
